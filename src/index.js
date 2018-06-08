ba35531722b029200b5f64dfb5a66930404d456f
John Salzman <john.r.salzman@gmail.com>
2018-06-08 18:29:35 -0400

    transfered header template from old site to react

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom";
 
 // Webpack won't resolve implicitly imported .jsx files, must specify exact file name
 // UPDATE: check webpack.config.js ==> resolve: ['*', '.js', '.jsx']
-import App from './components/App';
-import style from "./scss/main.scss"
+import Layout from './layouts/Layout'
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Layout />, document.getElementById("root"));
--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,8 +1,26 @@
-const Layout = () => (
-  <div id="page-layout">
+import React from 'react';
 
+const Layout = () => (
+  <div id="jrs-web">
+    <header>
+      <a id="header-primary-link" href="/WebDev" alt="home">
+        {/* JSX requires closing tag for self-contained elements, <img> => <img /> */}
+        <img src="/src/images/logo_max.svg" /> JRSweb
+      </a>
+      <nav id="header-nav">
+        <button>
+          <a href="/WebDev/routes/assignments.html">
+            Assignments
+        </a>
+        </button>
+        <button>
+          <a href="/WebDev/routes/about.html">
+            About
+        </a>
+        </button>
+      </nav>
+    </header>
   </div>
 );
-
 
 export default Layout;
