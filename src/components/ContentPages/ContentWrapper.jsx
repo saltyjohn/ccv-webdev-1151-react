73c229c90b29a9d64d7cf1bee5b3342949fcc058
John Salzman <john.r.salzman@gmail.com>
2018-06-13 12:23:07 -0400

    responsive header

---
+++ src/components/ContentPages/ContentWrapper.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const ContentWrapper = () => (
+  <div id="content-wrapper">
+    <article />
+  </div>
+);
+ContentWrapper.propTypes = {};
+
+export default ContentWrapper;
