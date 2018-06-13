1402a0f0a1359915c74ff791b8d7c4772381b240
John Salzman <john.r.salzman@gmail.com>
2018-06-13 13:00:13 -0400

    updated Header to have state and handle menu opening

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,36 +5,16 @@ import Header from './Header/Header';
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      menuFirstLoad: true,
-      menuOpen: false,
-    };
-    this.handleClick = this.handleClick.bind(this);
+    this.state = {};
+    // this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(e) {
-    const targetID = e.target.id;
-    switch (targetID) {
-      case 'menu-button':
-        this.setState({
-          menuFirstLoad: false,
-          menuOpen: !this.state.menuOpen,
-        });
-        break;
-      default:
-        break;
-    }
-  }
+  // handleClick(e) {}
 
   render() {
-    const openMenu = this.state.menuOpen;
     return (
       <div id="jrs-web">
-        <Header
-          menuOpen={openMenu}
-          onClick={this.handleClick}
-          showHide={this.state.menuFirstLoad ? '' : openMenu}
-        />
+        <Header />
         <article>
           {/* Placeholder for conten */}
           <h1>Hello, world!</h1>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
 
 import NameLogo from './components/Header.NameLogo';
 import MenuBurger from './components/Header.MenuBurger';
 import Menu from './components/Header.Menu';
 import './header.scss';
 
-const Header = props => (
-  <header role="banner">
-    <div className="menu-row">
-      <NameLogo />
-      <MenuBurger open={props.menuOpen} onClick={props.onClick} />
-    </div>
-    <Menu showHide={props.showHide} />
-  </header>
-);
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuFirstLoad: true,
+      menuOpen: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-Header.propTypes = {
-  onClick: PropTypes.func.isRequired,
-  menuOpen: PropTypes.bool.isRequired,
-  showHide: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]).isRequired,
-};
+  handleClick(e) {
+    const targetID = e.target.id;
+    switch (targetID) {
+      case 'menu-button':
+        this.setState({
+          menuFirstLoad: false,
+          menuOpen: !this.state.menuOpen,
+        });
+        break;
+      default:
+        break;
+    }
+  }
+
+  render() {
+    return (
+      <header role="banner">
+        <div className="menu-row">
+          <NameLogo />
+          <MenuBurger open={this.state.menuOpen} onClick={this.handleClick} />
+        </div>
+        <Menu showHide={this.state.menuFirstLoad ? '' : this.state.menuOpen} />
+      </header>
+    );
+  }
+}
 
 export default Header;
